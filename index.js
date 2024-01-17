ecea504859f7ae00d6587644dd2c13a1a0063e44
Einanas <inez.el.azzouzi@hva.nl>
2024-01-17 10:55:49 +0100

    added js code + filter code

---
+++ index.js
@@ -0,0 +1,17 @@
+// MICRO INTERACTION FILTER
+
+// Filter knop
+
+
+// Filter opties
+let fop = document.querySelector('')
+
+fop.addEventListener('click', filter )
+
+function fop() {
+    fop.classList.toggle('filter')
+}
+
+console.log(fop)
+
+// Team page double click animation Mobile First
