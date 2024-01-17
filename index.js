45407edbc532adaf81b96a4df4a390d706e28951
Einanas <inez.el.azzouzi@hva.nl>
2024-01-18 00:00:34 +0100

    New layout + added pictures to assets

--- index.js
+++ index.js
@@ -1,17 +1,58 @@
-// MICRO INTERACTION FILTER
+// get elements from dom
+const btns = document.querySelectorAll('.buttons button');
+const imgs = document.querySelectorAll('.images img');
 
-// Filter knop
-
-
-// Filter opties
-let fop = document.querySelector('')
-
-fop.addEventListener('click', filter )
-
-function fop() {
-    fop.classList.toggle('filter')
+// Add a click event to get all buttons
+for(let i = 1; i < btns.length; i++) {
+    btns[i].addEventListener('click', filterImg)
 }
 
-console.log(fop)
+// set active button on click
+function setActiveBtn(e) {
+    // Remeove active class from all buttons
+    btns.forEach(btn => {
+        btn.classList.remove('btn-clicked');
+    });
 
-// Team page double click animation Mobile First
+    // add active class to clicked button
+    e.target.classList.add('btn-clicked');
+}
+
+// Filter Images
+function filterImg(e) {
+    // Run the active button function
+    setActiveBtn(e);
+
+    // Loop through all images
+    imgs.forEach(img => {
+        // Expand all images
+        img.classList.remove('img-shrink');
+        img.classList.add('img-expand');
+
+        // Get data from data atributes
+        // Get image type data
+        const imgType = parseInt(img.dataset.img);
+        // Get button type data
+        const btnType = parseInt(e.target.dataset.btn);
+
+        // if the image and the type of the clicked
+        // button are NOT the same
+        if(imgType !== btnType) {
+            // hide the image
+            img.classList.remove('img-expand');
+            img.classList.add('img-shrink');
+        }
+    });
+}
+
+// Set click event for the 'All' button
+btns[0].addEventListener('click', (e) => {
+    // Run the active button function
+    setActiveBtn(e);
+    // Loop through all images
+    imgs.forEach(img => {
+        // Expand all images
+        img.classList.remove('img-shrink');
+        img.classList.add('img-expand');
+    });
+});
