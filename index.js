1ef6a0ffb768b33d80f79bdb17ef59cb0e870fc5
Einanas <inez.el.azzouzi@hva.nl>
2024-01-25 01:13:16 +0100

    mislukte werk ;)

--- index.js
+++ index.js
@@ -1,58 +1,36 @@
-// get elements from dom
-const btns = document.querySelectorAll('.buttons button');
-const imgs = document.querySelectorAll('.images img');
+// document.addEventListener('DOMContentLoaded', function () {
+//     const buttons = document.querySelectorAll('.buttons button');
+//     const imageContainers = document.querySelectorAll('.image-container');
 
-// Add a click event to get all buttons
-for(let i = 0; i < btns.length; i++) {
-    btns[i].addEventListener('click', filterImg)
-}
+//     buttons.forEach(button => {
+//       button.addEventListener('click', function () {
+//         const filterValue = this.getAttribute('data-btn');
 
-// set active button on click
-function setActiveBtn(e) {
-    // Remeove active class from all buttons
-    btns.forEach(btn => {
-        btn.classList.remove('btn-clicked');
-    });
+//         // Toon alle afbeeldingen als 'All' is geselecteerd
+//         if (filterValue === 'all') {
+//           imageContainers.forEach(container => {
+//             container.style.display = 'block';
+//           });
+//         } else {
+//           // Verberg afbeeldingen die niet overeenkomen met het geselecteerde filter
+//           imageContainers.forEach(container => {
+//             const dataImg = container.querySelector('.img').getAttribute('data-img');
+//             if (dataImg && dataImg.includes(filterValue)) {
+//               container.style.display = 'block';
+//             } else {
+//               container.style.display = 'none';
+//             }
+//           });
+//         }
 
-    // add active class to clicked button
-    e.target.classList.add('btn-clicked');
-}
+//         // Markeer het geselecteerde filter
+//         buttons.forEach(btn => {
+//           btn.classList.remove('btn-clicked');
+//         });
+//         this.classList.add('btn-clicked');
+//       });
+//     });
+//   });
 
-// Filter Images
-function filterImg(e) {
-    // Run the active button function
-    setActiveBtn(e);
 
-    // Loop through all images
-    imgs.forEach(img => {
-        // Expand all images
-        img.classList.remove('img-shrink');
-        img.classList.add('img-expand');
-
-        // Get data from data atributes
-        // Get image type data
-        const imgType = parseInt(img.dataset.img);
-        // Get button type data
-        const btnType = parseInt(e.target.dataset.btn);
-
-        // if the image and the type of the clicked
-        // button are NOT the same
-        if(imgType !== btnType) {
-            // hide the image
-            img.classList.remove('img-expand');
-            img.classList.add('img-shrink');
-        }
-    });
-}
-
-// Set click event for the 'All' button
-btns[0].addEventListener('click', (e) => {
-    // Run the active button function
-    setActiveBtn(e);
-    // Loop through all images
-    imgs.forEach(img => {
-        // Expand all images
-        img.classList.remove('img-shrink');
-        img.classList.add('img-expand');
-    });
-});
+  
